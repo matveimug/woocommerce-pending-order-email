0ff5816fa33464efdf84ded993fd183433041eaa
matveimug <madis.vaher@gmail.com>
2019-09-01 17:55:33 +0300

    maby work????

--- includes/class-wc-pending-order-email.php
+++ includes/class-wc-pending-order-email.php
@@ -1,182 +1,206 @@
 <?php
-
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
-
 /**
- * A custom Pending Order WooCommerce Email class
+ * Class WC_Email_Pending_Order file
  *
- * @since 0.1
- * @extends \WC_Email
+ * @package WooCommerce\Emails
  */
-class WC_Pending_Order_Email extends WC_Email {
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
-	/**
-	 * Set email defaults
-	 *
-	 * @since 0.1
-	 */
-	public function __construct() {
-
-		// set ID, this simply needs to be a unique name
-		$this->id = 'wc_pending_order';
-
-		// this is the title in WooCommerce Email settings
-		$this->title = 'Pending Order';
-
-		// this is the description in WooCommerce email settings
-		$this->description = 'Pending Order Notification emails are sent when a customer places an order';
-
-		// these define the locations of the templates that this email should use, we'll just use the new order template since this email is similar
-		$this->template_html  = 'emails/admin-new-order.php';
-		$this->template_plain = 'emails/plain/admin-new-order.php';
-
-		// Trigger on thankyou
-//		add_action( 'woocommerce_checkout_order_processed', array( $this, 'trigger' ) );
-		add_action( 'woocommerce_new_order', array( $this, 'trigger' ), 10, 2 );
-
-		// Call parent constructor to load any other defaults not explicity defined here
-		parent::__construct();
-
-		// this sets the recipient to the settings defined below in init_form_fields()
-		$this->recipient = $this->get_option( 'recipient' );
-
-		// if none was entered, just use the WP admin email as a fallback
-		if ( ! $this->recipient )
-			$this->recipient = get_option( 'admin_email' );
-	}
+if ( ! class_exists( 'WC_Email_Pending_Order' ) ) :
 
 	/**
-	 * Get email subject.
+	 * Pending Order Email.
 	 *
-	 * @since  3.1.0
-	 * @return string
-	 */
-	public function get_default_subject() {
-		return __( '[{site_title}]: New order #{order_number}', 'woocommerce' );
-	}
-
-	/**
-	 * Get email heading.
+	 * An email sent to the admin when a pending order is received/paid for.
 	 *
-	 * @since  3.1.0
-	 * @return string
+	 * @class       WC_Email_Pending_Order
+	 * @version     2.0.0
+	 * @package     WooCommerce/Classes/Emails
+	 * @extends     WC_Email
 	 */
-	public function get_default_heading() {
-		return __( 'New Order: #{order_number}', 'woocommerce' );
-	}
+	class WC_Email_Pending_Order extends WC_Email {
 
-	/**
-	 * Determine if the email should actually be sent and setup email merge variables
-	 *
-	 * @since 0.1
-	 * @param int $order_id
-	 */
-	public function trigger( $order_id ) {
+		/**
+		 * Constructor.
+		 */
+		public function __construct() {
+			$this->id             = 'pending_order';
+			$this->title          = __( 'Pending order', 'woocommerce' );
+			$this->description    = __( 'Pending order emails are sent to chosen recipient(s) when a new order is received.', 'woocommerce' );
+			$this->template_html  = 'emails/admin-new-order.php';
+			$this->template_plain = 'emails/plain/admin-new-order.php';
+			$this->placeholders   = array(
+				'{order_date}'   => '',
+				'{order_number}' => '',
+			);
 
-		// bail if no order ID is present
-		if ( ! $order_id )
-			return;
+			// Triggers for this email.
+			add_action( 'woocommerce_new_order', array( $this, 'trigger' ), 10, 2 );
 
-		// setup order object
-		$this->object = new WC_Order( $order_id );
+			// Call parent constructor.
+			parent::__construct();
 
-		// replace variables in the subject/headings
-		$this->find[] = '{order_date}';
-		$this->replace[] = date_i18n( woocommerce_date_format(), strtotime( $this->object->order_date ) );
+			// Other settings.
+			$this->recipient = $this->get_option( 'recipient', get_option( 'admin_email' ) );
+		}
 
-		$this->find[] = '{order_number}';
-		$this->replace[] = $this->object->get_order_number();
+		/**
+		 * Get email subject.
+		 *
+		 * @since  3.1.0
+		 * @return string
+		 */
+		public function get_default_subject() {
+			return __( '[{site_title}]: Pending order #{order_number}', 'woocommerce' );
+		}
 
-		if ( ! $this->is_enabled() || ! $this->get_recipient() )
-			return;
+		/**
+		 * Get email heading.
+		 *
+		 * @since  3.1.0
+		 * @return string
+		 */
+		public function get_default_heading() {
+			return __( 'Pending Order: #{order_number}', 'woocommerce' );
+		}
 
-		// woohoo, send the email!
-		$this->send( $this->get_recipient(), $this->get_subject(), $this->get_content(), $this->get_headers(), $this->get_attachments() );
-	}
+		/**
+		 * Trigger the sending of this email.
+		 *
+		 * @param int            $order_id The order ID.
+		 * @param WC_Order|false $order Order object.
+		 */
+		public function trigger( $order_id, $order = false ) {
+			$this->setup_locale();
 
+			if ( $order_id && ! is_a( $order, 'WC_Order' ) ) {
+				$order = wc_get_order( $order_id );
+			}
 
-	/**
-	 * get_content_html function.
-	 *
-	 * @since 0.1
-	 * @return string
-	 */
-	public function get_content_html() {
-		ob_start();
-		woocommerce_get_template( $this->template_html, array(
-			'order'         => $this->object,
-			'email_heading' => $this->get_heading()
-		) );
-		return ob_get_clean();
-	}
+			if ( is_a( $order, 'WC_Order' ) ) {
+				$this->object                         = $order;
+				$this->placeholders['{order_date}']   = wc_format_datetime( $this->object->get_date_created() );
+				$this->placeholders['{order_number}'] = $this->object->get_order_number();
+			}
 
+			if ( $this->is_enabled() && $this->get_recipient() ) {
+				$this->send( $this->get_recipient(), $this->get_subject(), $this->get_content(), $this->get_headers(), $this->get_attachments() );
+			}
 
-	/**
-	 * get_content_plain function.
-	 *
-	 * @since 0.1
-	 * @return string
-	 */
-	public function get_content_plain() {
-		ob_start();
-		woocommerce_get_template( $this->template_plain, array(
-			'order'         => $this->object,
-			'email_heading' => $this->get_heading()
-		) );
-		return ob_get_clean();
-	}
+			$this->restore_locale();
+		}
 
-
-	/**
-	 * Initialize Settings Form Fields
-	 *
-	 * @since 2.0
-	 */
-	public function init_form_fields() {
-
-		$this->form_fields = array(
-			'enabled'    => array(
-				'title'   => 'Enable/Disable',
-				'type'    => 'checkbox',
-				'label'   => 'Enable this email notification',
-				'default' => 'yes'
-			),
-			'recipient'  => array(
-				'title'       => 'Recipient(s)',
-				'type'        => 'text',
-				'description' => sprintf( 'Enter recipients (comma separated) for this email. Defaults to <code>%s</code>.', esc_attr( get_option( 'admin_email' ) ) ),
-				'placeholder' => '',
-				'default'     => ''
-			),
-			'subject'    => array(
-				'title'       => 'Subject',
-				'type'        => 'text',
-				'description' => sprintf( 'This controls the email subject line. Leave blank to use the default subject: <code>%s</code>.', $this->subject ),
-				'placeholder' => '',
-				'default'     => ''
-			),
-			'heading'    => array(
-				'title'       => 'Email Heading',
-				'type'        => 'text',
-				'description' => sprintf( __( 'This controls the main heading contained within the email notification. Leave blank to use the default heading: <code>%s</code>.' ), $this->heading ),
-				'placeholder' => '',
-				'default'     => ''
-			),
-			'email_type' => array(
-				'title'       => 'Email type',
-				'type'        => 'select',
-				'description' => 'Choose which format of email to send.',
-				'default'     => 'html',
-				'class'       => 'email_type',
-				'options'     => array(
-					'plain'	    => __( 'Plain text', 'woocommerce' ),
-					'html' 	    => __( 'HTML', 'woocommerce' ),
-					'multipart' => __( 'Multipart', 'woocommerce' ),
+		/**
+		 * Get content html.
+		 *
+		 * @return string
+		 */
+		public function get_content_html() {
+			return wc_get_template_html(
+				$this->template_html,
+				array(
+					'order'              => $this->object,
+					'email_heading'      => $this->get_heading(),
+					'additional_content' => $this->get_additional_content(),
+					'sent_to_admin'      => true,
+					'plain_text'         => false,
+					'email'              => $this,
 				)
-			)
-		);
+			);
+		}
+
+		/**
+		 * Get content plain.
+		 *
+		 * @return string
+		 */
+		public function get_content_plain() {
+			return wc_get_template_html(
+				$this->template_plain,
+				array(
+					'order'              => $this->object,
+					'email_heading'      => $this->get_heading(),
+					'additional_content' => $this->get_additional_content(),
+					'sent_to_admin'      => true,
+					'plain_text'         => true,
+					'email'              => $this,
+				)
+			);
+		}
+
+		/**
+		 * Default content to show below main email content.
+		 *
+		 * @since 3.7.0
+		 * @return string
+		 */
+		public function get_default_additional_content() {
+			return __( 'Congratulations on the sale.', 'woocommerce' );
+		}
+
+		/**
+		 * Initialise settings form fields.
+		 */
+		public function init_form_fields() {
+			/* translators: %s: list of placeholders */
+			$placeholder_text  = sprintf( __( 'Available placeholders: %s', 'woocommerce' ), '<code>' . implode( '</code>, <code>', array_keys( $this->placeholders ) ) . '</code>' );
+			$this->form_fields = array(
+				'enabled'    => array(
+					'title'   => __( 'Enable/Disable', 'woocommerce' ),
+					'type'    => 'checkbox',
+					'label'   => __( 'Enable this email notification', 'woocommerce' ),
+					'default' => 'yes',
+				),
+				'recipient'  => array(
+					'title'       => __( 'Recipient(s)', 'woocommerce' ),
+					'type'        => 'text',
+					/* translators: %s: WP admin email */
+					'description' => sprintf( __( 'Enter recipients (comma separated) for this email. Defaults to %s.', 'woocommerce' ), '<code>' . esc_attr( get_option( 'admin_email' ) ) . '</code>' ),
+					'placeholder' => '',
+					'default'     => '',
+					'desc_tip'    => true,
+				),
+				'subject'    => array(
+					'title'       => __( 'Subject', 'woocommerce' ),
+					'type'        => 'text',
+					'desc_tip'    => true,
+					'description' => $placeholder_text,
+					'placeholder' => $this->get_default_subject(),
+					'default'     => '',
+				),
+				'heading'    => array(
+					'title'       => __( 'Email heading', 'woocommerce' ),
+					'type'        => 'text',
+					'desc_tip'    => true,
+					'description' => $placeholder_text,
+					'placeholder' => $this->get_default_heading(),
+					'default'     => '',
+				),
+				'additional_content' => array(
+					'title'       => __( 'Additional content', 'woocommerce' ),
+					'description' => __( 'Text to appear below the main email content.', 'woocommerce' ) . ' ' . $placeholder_text,
+					'css'         => 'width:400px; height: 75px;',
+					'placeholder' => __( 'N/A', 'woocommerce' ),
+					'type'        => 'textarea',
+					'default'     => $this->get_default_additional_content(),
+					'desc_tip'    => true,
+				),
+				'email_type' => array(
+					'title'       => __( 'Email type', 'woocommerce' ),
+					'type'        => 'select',
+					'description' => __( 'Choose which format of email to send.', 'woocommerce' ),
+					'default'     => 'html',
+					'class'       => 'email_type wc-enhanced-select',
+					'options'     => $this->get_email_type_options(),
+					'desc_tip'    => true,
+				),
+			);
+		}
 	}
 
+endif;
 
-} // end \WC_Pending_Order_Email class
+return new WC_Email_Pending_Order();
